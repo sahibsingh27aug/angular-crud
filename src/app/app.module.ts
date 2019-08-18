f01dd306042bcb31c303b38d5a0e4f718a5bab4d
SahibSinghBanga <sahib_singh_1997@hotmail.com>
2019-08-18 09:32:19 +0530

    Final Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PostsComponent } from './components/posts/posts.component';
@@ -22,6 +23,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 @NgModule({
   declarations: [
     AppComponent,
+    UserComponent,
     UsersComponent,
     NavbarComponent,
     PostsComponent,
